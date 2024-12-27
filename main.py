df2c0893a0cb64f215224983688c7be45800b5f8
Matías Szostak <93807332+matiasmszostak@users.noreply.github.com>
2024-12-26 23:55:07 -0300

    generate_password function created.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
-from tkinter.messagebox import askokcancel
+from random import choice, randint, shuffle
+import pyperclip
 
 
-# ---------------------------- UI SETUP ------------------------------- #
 
+# ---------------------------- GENERATE PASSWORD ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def generate_password():
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+
+    pyperclip.copy(password)
+    print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -70,7 +90,7 @@ password_entry = Entry(width=22)
 password_entry.grid(column=1, row=3, sticky="W", pady=5)
 
 # Buttons
-generate_pass_button = Button(text="Generate Password", width=14)
+generate_pass_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_pass_button.grid(column=2, row=3, sticky="W", padx=5)
 add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2, pady=10)
